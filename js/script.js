615d9b32a93bb75ad312e63eef99474547eaa8ce
Risto Tõldsep <r.toldsep@gmail.com>
2023-02-24 19:18:26 +0200

    Final update

--- js/script.js
+++ js/script.js
@@ -28,7 +28,8 @@ const countryFlagEndpoint = 'https://countryflagsapi.com/png/';
 //Array for weather images (to make the main image dynamic) (IDs from openweathermap api home page)
 let weatherImages = [
   {
-    url: "../images/clear-sky.png",
+    // LOCALLY => url: "../images/clear-sky.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/clear-sky.png",
     ids: [800],
   },
   {
@@ -36,31 +37,31 @@ let weatherImages = [
     ids: [803, 804],
   },
   {
-    url: "../images/few-clouds.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/few-clouds.png",
     ids: [801],
   },
   {
-    url: "../images/mist.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/mist.png",
     ids: [701, 711, 721, 731, 741, 751, 761, 762, 771, 781],
   },
   {
-    url: "../images/rain.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/rain.png",
     ids: [500, 501, 502, 503, 504],
   },
   {
-    url: "../images/scattered-clouds.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/scattered-clouds.png",
     ids: [802],
   },
   {
-    url: "../images/shower-rain.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/shower-rain.png",
     ids: [520, 521, 522, 531, 300, 301, 302, 310, 311, 312, 313, 314, 321],
   },
   {
-    url: "../images/snow.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/snow.png",
     ids: [511, 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622],
   },
   {
-    url: "../images/thunderstorm.png",
+    url: "https://ristotoldsep.github.io/WeatherApp/images/thunderstorm.png",
     ids: [200, 201, 202, 210, 211, 212, 221, 230, 231, 232],
   },
 ];
