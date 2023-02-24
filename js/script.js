842678c4926f9a4538a299d48fb4b7d0a87c208c
Risto Tõldsep <r.toldsep@gmail.com>
2023-02-24 19:16:04 +0200

    update test

--- js/script.js
+++ js/script.js
@@ -27,43 +27,42 @@ const countryFlagEndpoint = 'https://countryflagsapi.com/png/';
 
 //Array for weather images (to make the main image dynamic) (IDs from openweathermap api home page)
 let weatherImages = [
-    {
-        url: '../images/clear-sky.png',
-        ids: [800]
-    },
-    {
-        url: '/images/broken-clouds.png',
-        ids: [803, 804]
-    },
-    {
-        url: '../images/few-clouds.png',
-        ids: [801]
-    },
-    {
-        url: '../images/mist.png',
-        ids: [701, 711, 721, 731, 741, 751, 761, 762, 771, 781]
-    },
-    {
-        url: '../images/rain.png',
-        ids: [500, 501, 502, 503, 504]
-    },
-    {
-        url: '../images/scattered-clouds.png',
-        ids: [802]
-    },
-    {
-        url: '../images/shower-rain.png',
-        ids: [520, 521, 522, 531, 300, 301, 302, 310, 311, 312, 313, 314, 321]
-    },
-    {
-        url: '../images/snow.png',
-        ids: [511, 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622]
-    },
-    {
-        url: '../images/thunderstorm.png',
-        ids: [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]
-    },
-   
+  {
+    url: "../images/clear-sky.png",
+    ids: [800],
+  },
+  {
+    url: "https://ristotoldsep.github.io/WeatherApp/images/broken-clouds.png",
+    ids: [803, 804],
+  },
+  {
+    url: "../images/few-clouds.png",
+    ids: [801],
+  },
+  {
+    url: "../images/mist.png",
+    ids: [701, 711, 721, 731, 741, 751, 761, 762, 771, 781],
+  },
+  {
+    url: "../images/rain.png",
+    ids: [500, 501, 502, 503, 504],
+  },
+  {
+    url: "../images/scattered-clouds.png",
+    ids: [802],
+  },
+  {
+    url: "../images/shower-rain.png",
+    ids: [520, 521, 522, 531, 300, 301, 302, 310, 311, 312, 313, 314, 321],
+  },
+  {
+    url: "../images/snow.png",
+    ids: [511, 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622],
+  },
+  {
+    url: "../images/thunderstorm.png",
+    ids: [200, 201, 202, 210, 211, 212, 221, 230, 231, 232],
+  },
 ];
 
 //Function which gets the city
